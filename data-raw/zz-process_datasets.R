d3141fac7c24bfc6f00a92f7d4517880317a9f8b
Richard Iannone <riannone@me.com>
2020-06-03 23:55:38 -0400

    Update zz-process_datasets.R

--- data-raw/zz-process_datasets.R
+++ data-raw/zz-process_datasets.R
@@ -3,7 +3,6 @@ library(usethis)
 source("data-raw/01-generate_intendo_data.R")
 
 # Create external datasets
-
 usethis::use_data(
   intendo_users, intendo_revenue, intendo_daily_users,
   internal = FALSE, overwrite = TRUE
