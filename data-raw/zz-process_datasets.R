8a2b813c2552f6f831e25b33bc30ce293934a610
Richard Iannone <riannone@me.com>
2021-01-04 09:31:51 -0500

    Update zz-process_datasets.R

--- data-raw/zz-process_datasets.R
+++ data-raw/zz-process_datasets.R
@@ -1,7 +1,5 @@
 library(usethis)
 
-# source("data-raw/01-generate_intendo_data-2.R")
-
 intendo_users <- readRDS("data-raw/intendo_users.rds")
 intendo_revenue <- readRDS("data-raw/intendo_revenue.rds")
 intendo_daily_users <- readRDS("data-raw/intendo_daily_users.rds")
@@ -9,10 +7,11 @@ intendo_daily_users <- readRDS("data-raw/intendo_daily_users.rds")
 users_daily <- readRDS("data-raw/users_daily.rds")
 all_revenue <- readRDS("data-raw/all_revenue.rds")
 all_sessions <- readRDS("data-raw/all_sessions.rds")
+user_summary <- readRDS("data-raw/user_summary.rds")
 
 # Create external datasets
 usethis::use_data(
   intendo_users, intendo_revenue, intendo_daily_users,
-  users_daily, all_revenue, all_sessions,
+  users_daily, all_revenue, all_sessions, user_summary,
   internal = FALSE, overwrite = TRUE
 )
