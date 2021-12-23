5f37c96ed66590b896baf78abebf64895733b25f
Richard Iannone <riannone@me.com>
2021-12-23 16:30:41 -0500

    Update 02-generate_small_intendo_data.R

--- data-raw/02-generate_small_intendo_data.R
+++ data-raw/02-generate_small_intendo_data.R
@@ -3,10 +3,10 @@ library(tidyverse)
 # Set a seed
 set.seed(23)
 
-users_daily_large <- readRDS("data-large/users_daily_large.rds")
-all_revenue_large <- readRDS("data-large/all_revenue_large.rds")
-all_sessions_large <- readRDS("data-large/all_sessions_large.rds")
-user_summary_large <- readRDS("data-large/user_summary_large.rds")
+users_daily_large <- readRDS("data-large/sj_users_daily_large.rds")
+all_revenue_large <- readRDS("data-large/sj_all_revenue_large.rds")
+all_sessions_large <- readRDS("data-large/sj_all_sessions_large.rds")
+user_summary_large <- readRDS("data-large/sj_user_summary_large.rds")
 
 users_basis <-
   user_summary_large %>%
@@ -35,7 +35,7 @@ user_summary_small <-
   user_summary_large %>%
   dplyr::filter(player_id %in% users_small)
 
-saveRDS(users_daily_small, file = "data-raw/sj_users_daily.rds")
-saveRDS(all_revenue_small, file = "data-raw/sj_all_revenue.rds")
-saveRDS(all_sessions_small, file = "data-raw/sj_all_sessions.rds")
-saveRDS(user_summary_small, file = "data-raw/sj_user_summary.rds")
+saveRDS(users_daily_small, file = "data-small/sj_users_daily_small.rds")
+saveRDS(all_revenue_small, file = "data-small/sj_all_revenue_small.rds")
+saveRDS(all_sessions_small, file = "data-small/sj_all_sessions_small.rds")
+saveRDS(user_summary_small, file = "data-small/sj_user_summary_small.rds")
