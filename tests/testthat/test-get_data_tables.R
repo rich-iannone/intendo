fef2bf9914402fe8d8bfd50da724f2eb2d55d537
Richard Iannone <riannone@me.com>
2021-12-26 12:39:00 -0500

    Update test-get_data_tables.R

--- tests/testthat/test-get_data_tables.R
+++ tests/testthat/test-get_data_tables.R
@@ -1,6 +1,10 @@
 skip_on_cran()
 
-test_that("getting a data table works", {
+test_that("getting the different data tables works", {
+
+  #
+  # Obtaining the small versions of the tables
+  #
 
   all_sessions_tbl <- all_sessions(size = "small")
   expect_type(all_sessions_tbl, "list")
@@ -13,4 +17,20 @@ test_that("getting a data table works", {
 
   user_summary_tbl <- user_summary(size = "small")
   expect_type(user_summary_tbl, "list")
+
+  #
+  # Obtaining the small and faulty versions of the tables
+  #
+
+  all_sessions_f_tbl <- all_sessions(size = "small", quality = "faulty")
+  expect_type(all_sessions_f_tbl, "list")
+
+  all_revenue_f_tbl <- all_revenue(size = "small", quality = "faulty")
+  expect_type(all_revenue_f_tbl, "list")
+
+  users_daily_f_tbl <- users_daily(size = "small", quality = "faulty")
+  expect_type(users_daily_f_tbl, "list")
+
+  user_summary_f_tbl <- user_summary(size = "small", quality = "faulty")
+  expect_type(user_summary_f_tbl, "list")
 })
