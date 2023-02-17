1700026fd8b2cc10aab830aef3b38cbf2b9f2d2b
Richard Iannone <riannone@me.com>
2023-02-17 09:36:51 -0500

    Update zz-process_datasets.R

--- data-raw/zz-process_datasets.R
+++ data-raw/zz-process_datasets.R
@@ -1,9 +1,14 @@
 library(usethis)
 
-source("data-raw/05-generate-metadata.R")
+source("data-raw/X01-generate-metadata.R")
+source("data-raw/X01-generate-previews.R")
 
 # Create internal dataset
 usethis::use_data(
   intendo_meta,
+  all_revenue_preview,
+  users_daily_preview,
+  user_summary_preview,
+  all_sessions_preview,
   internal = TRUE, overwrite = TRUE
 )
